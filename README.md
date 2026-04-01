c68d4090b6e1a28d789f5cdbab901a29d0c9c213
heyitskdk <keshav.duttkaushik23@gmail.com>
2026-04-01 15:54:54 +0530

    i have a dream

---
+++ README.md
@@ -0,0 +1,10 @@
+# the dream.
+
+hello and welcome to my dream, and my life while at it
+
+catch me grinding my fingers off as I power through life's challenges and secure a spot in the world's<br />
+top product based companies.
+
+Stripe, Rubrik and Google being my stand out favourites though (manifesting hard)
+
+Thanks for checking my work out, in case you did :)
---
+++ src/main/java/org/heyitskdk/Dream.java
@@ -0,0 +1,28 @@
+package org.heyitskdk;
+
+public class Dream {
+
+    public static void main(String[] args) {
+        String i_have_a_dream = """
+                =====================================================================
+               \s
+                                     "I have a dream" by KDK
+                       \s
+                I have a dream that one day down in my life, with it's vicious past,
+                with it's wound dripping with abuse and confusion - one day right here
+                as I breathe, will become the most successful man on earth.
+                I have a dream today.
+                I have a dream that I shall find love, the way love never found me.
+                Every tear of exhaustion and frustration wil be made plain, and the
+                crooked places will be made straight, and the glory of Shree Krishna
+                will be revealed and all victims shall see it together.
+               \s
+                This is my hope, and is the faith I start this journey with. The faith
+                that I will be able to transform the jangling hurt into a beautiful
+                melody from God.
+               
+                =====================================================================
+               \s""";
+        System.out.println(i_have_a_dream);
+    }
+}
